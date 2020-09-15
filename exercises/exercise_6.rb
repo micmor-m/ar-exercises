db0375075ef9560d52e4fef0e069e70917b8a10f
micmor-m <micmor.m@gmail.com>
2020-09-15 14:51:24 +0000

    Done exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,22 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+class Store
+  has_many :employees
+end
+ 
+class Employee
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Jonh", last_name: "Red", hourly_rate: 50)
+@store2.employees.create(first_name: "Bob", last_name: "White", hourly_rate: 70)
+@store2.employees.create(first_name: "Lucas", last_name: "Brown", hourly_rate: 60)
+@store4.employees.create(first_name: "Emily", last_name: "Smith", hourly_rate: 55)
+@store4.employees.create(first_name: "Susan", last_name: "Shumaker", hourly_rate: 60)
+@store5.employees.create(first_name: "Andy", last_name: "Edison", hourly_rate: 80)
+@store5.employees.create(first_name: "Tom", last_name: "Green", hourly_rate: 67)
+@store6.employees.create(first_name: "Tim", last_name: "Horton", hourly_rate: 61)
+@store1.employees.create(first_name: "Sara", last_name: "Speck", hourly_rate: 76)
