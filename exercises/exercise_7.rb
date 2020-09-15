78791634015954cc97dab237906b02a5cb657fef
micmor-m <micmor.m@gmail.com>
2020-09-15 15:27:32 +0000

    Done exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,23 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200}
+  validates :store_id, presence: true
+end
+
+class Store
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+end
+
+puts "PLease insert a store name:"
+userstore = $stdin.gets.chomp
+p "userstore #{userstore}"
+
+#@store4 = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+@store7 = Store.create(name: "#{userstore}")
+
+puts @store7.errors.full_messages
